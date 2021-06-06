33afe8aff8d9f8ef567effcb74f19d4e023f8f09
jackteruya <jack.teruya@gmail.com>
2021-06-05 21:27:19 -0400

    Correcao setting

--- desafio_frexco_django/settings.py
+++ desafio_frexco_django/settings.py
@@ -78,6 +78,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'desafio_frexco_cliente.modulos.context_processors.listar_modulos'
             ],
         },
     },
