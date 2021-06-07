e3877922f9f5f5988f7957978969219545eb4a93
jackteruya <jack.teruya@gmail.com>
2021-06-07 06:19:30 -0400

    atualização settings

--- desafio_frexco_django/settings.py
+++ desafio_frexco_django/settings.py
@@ -53,17 +53,14 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
-
-MIDDLEWARE_CLASSES = (
-    'whitenoise.middleware.WhiteNoiseMiddleware',)
 
 ROOT_URLCONF = 'desafio_frexco_django.urls'
 
