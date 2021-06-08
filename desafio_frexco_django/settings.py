a330fbbaccb250064391e032dd699f375b4161bd
jackteruya <jack.teruya@gmail.com>
2021-06-07 22:06:09 -0400

    Atualizando aplicacao settings

--- desafio_frexco_django/settings.py
+++ desafio_frexco_django/settings.py
@@ -39,6 +39,7 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # Application definition
 
 INSTALLED_APPS = [
+    'whitenoise.runserver_nostatic',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
